558666bb3c16cf7904348bb40d5da1d889ce1f4b
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-11-27 18:21:49 -0500

    Add project files.

---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,23 @@
+﻿//https://exercism.org/tracks/csharp/exercises/error-handling/edit
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception("throws an exception.");
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        return null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
